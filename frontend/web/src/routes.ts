94c0eccc041d52e1d7d32eeff50a4e1152917de4
yoandraach <yoandraach@users.noreply.github.com>
2025-11-06 16:07:44 +0000

    Add routes.ts, sms.jsx

---
+++ frontend/web/src/routes.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const usevalidator_config = () => { return {}; };
+
+
---
+++ frontend/web/src/sms.jsx
@@ -0,0 +1,5 @@
+import { ethers } from 'ethers';
+
+export const usesms_mock = () => { return {}; };
+
+
